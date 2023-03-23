703a2f27fff398cfe66d203e11a784a672f0ae2d
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-03-23 01:58:44 -0400

    Initial Commit

---
+++ GitHub Example/GitHub_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExampleApp.swift
+//  GitHub Example
+//
+//  Created by Lori Rothermel on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
